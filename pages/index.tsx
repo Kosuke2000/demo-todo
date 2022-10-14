1f58be9dab2120a6b776cfe6f8c6140386203c1f
Kosuke2000 <youuchiharu@gmail.com>
2022-10-14 13:38:47 +0900

    feat: 😙Todoコンポーネントの雛形作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,36 +35,111 @@ const mockTodoList = [mockTodo0, mockTodo1, mockTodo2, mockTodo3]
 
 let nextId = 4
 
+interface TodoProps {
+  todo: Todo
+  onToggle: (id: number) => void
+  onRemove: (id: number) => void
+  onEdit: (id: number, name: string) => void
+}
+
+const Todo = ({ todo, onToggle, onRemove, onEdit }: TodoProps) => {
+  const [isEdit, setIsEditing] = useState(false)
+  const [text, setText] = useState(todo.name)
+
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }
+
+  // 編集モードにする
+  const start = () => setIsEditing(true)
+  // 編集舌テキストを新たに登録し、編集モードを終了する
+  const finish = () => {
+    onEdit(todo.id, text)
+
+    setIsEditing(false)
+  }
+
+  const todoName = !isEdit ? (
+    <>
+      <td className="py-4 px-6">{todo.name}</td>
+      <td>
+        <button onClick={start}>編集</button>
+      </td>
+      <td className="py-4 px-6">
+        <button onClick={() => onRemove(todo.id)} className="bg-red-100">
+          削除
+        </button>
+      </td>
+    </>
+  ) : (
+    <>
+      <td>
+        <input value={text} onChange={handleChangeInput} className="border" />
+      </td>
+      <td>
+        <button onClick={finish} className="bg-blue-300">
+          編集完了
+        </button>
+      </td>
+    </>
+  )
+
+  return (
+    <tr>
+      <td className="py-4 px-6">
+        <input
+          onClick={() => onToggle(todo.id)}
+          type="checkbox"
+          checked={todo.isDone}
+        />
+      </td>
+      {todoName}
+    </tr>
+  )
+}
+
 const Home: NextPage = () => {
   // todoListというステートを作成する
   const [todoList, setTodoList] = useState(mockTodoList)
   const [text, setText] = useState("")
 
-  // text を更新する関数
-  const newtext = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value)
   }
 
-  // textをコンソールに表示
-  console.log(text)
-
-  // テキストを受け取り、todoListにTodoを追加する関数
-  const register = () => {
-    // 入力欄の内容を持ったnewTodoが作成される
+  const create = () => {
     const newTodo: Todo = {
       id: nextId,
       name: text,
       isDone: false,
     }
 
-    // newTodoをtodoListに追加する
     setTodoList([...todoList, newTodo])
 
-    // 入力欄をクリアする
     setText("")
-
-    // IDを１つ大きくする
     nextId++
+  }
+
+  const remove = (id: number) => {
+    const newState = todoList.filter((todo) => todo.id !== id)
+    setTodoList(newState)
+  }
+
+  const toggle = (id: number) => {
+    const newState = todoList.map((todo) => {
+      if (todo.id !== id) return todo
+
+      return { ...todo, isDone: !todo.isDone }
+    })
+    setTodoList(newState)
+  }
+
+  const clean = () => {
+    return
+  }
+
+  const edit = (id: number, name: string) => {
+    return
   }
 
   return (
@@ -79,34 +154,39 @@ const Home: NextPage = () => {
         <h1>Todoアプリ</h1>
 
         <div>
-          <input value={text} onChange={newtext} className="border"></input>
-          <button onClick={register}>todo登録</button>
+          <input
+            value={text}
+            onChange={handleChangeInput}
+            className="my-4 border"
+          ></input>
+          <button onClick={create} className="bg-slate-100">
+            Todoを追加
+          </button>
         </div>
 
         <table className="text-sm text-left text-gray-500 table-auto">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
-            {/* mockTodoをmapを使って表示する */}
             <tr>
               <th className="py-4 px-6">ステータス</th>
               <th className="py-4 px-6">名前</th>
               <th></th>
-              <th className="py-4 px-6"></th>
+              <th className="py-4 px-6">
+                <button onClick={clean} className="bg-red-600">
+                  一掃
+                </button>
+              </th>
             </tr>
           </thead>
           <tbody>
-            {todoList.map((todo) => {
-              return (
-                <tr
-                  key={todo.id}
-                  className="bg-white dark:bg-gray-800 border-b"
-                >
-                  <td></td>
-                  <td className="py-4 px-6"></td>
-                  <td>{todo.isDone}</td>
-                  <td>{todo.name}</td>
-                </tr>
-              )
-            })}
+            {todoList.map((todo) => (
+              <Todo
+                key={todo.id}
+                todo={todo}
+                onToggle={toggle}
+                onRemove={remove}
+                onEdit={edit}
+              />
+            ))}
           </tbody>
         </table>
       </main>
